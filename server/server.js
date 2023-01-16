0baffbfaa7ad83ba90dcda8aa3613ac985058c0f
Mohamed <mohamed.sheik98@outlook.com>
2023-01-16 17:57:44 +0000

    update

--- server/server.js
+++ server/server.js
@@ -35,8 +35,8 @@ app.use(express.json({ limit: "30mb" }));
 app.use(
   cors({
     origin: [
-      "http://blen-blog.netlify.app/",
-      "http://localhost:3000",
+      "https://blen-blog.netlify.app/",
+      "https://localhost:3000",
       "https://api.cloudinary.com/v1_1/dxjwlxupx/image/upload",
       "https://blen.onrender.com",
     ],
