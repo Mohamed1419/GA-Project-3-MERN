781054654dda7f91685614242127cedcd78b8d0c
Mohamed <mohamed.sheik98@outlook.com>
2023-01-19 00:34:55 +0000

    modify netlify url on cors origins

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ app.use(express.json({ limit: "30mb" }));
 app.use(
   cors({
     origin: [
-      "https://blen-blog.netlify.app/",
+      "https://blen-blog.netlify.app",
       "http://localhost:3001",
       "http://localhost:3000",
       "https://api.cloudinary.com/v1_1/dxjwlxupx/image/upload",
